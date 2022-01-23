4a94f1b3a017be50c29a76e3d14a1e58567dbe47
takahashi-1234 <riki.1130@icloud.com>
2022-01-23 23:54:27 +0000

    [add]いいね機能の非同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,11 @@ class FavoritesController < ApplicationController
   def create
     favorite=current_user.favorites.new(post_id:@post.id)
     favorite.save
-    redirect_back(fallback_location:root_path)
   end
   
   def destroy
     favorite=current_user.favorites.find_by(post_id:@post.id)
     favorite.destroy
-    redirect_back(fallback_location:root_path)
   end
   
   private
